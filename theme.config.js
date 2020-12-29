2214f785b4414f37def5e89021ebf0f0a05b8c8e
steelydylan <ess_president@me.com>
2020-12-29 23:10:44 +0900

    chore: trivial fix

--- theme.config.js
+++ theme.config.js
@@ -15,7 +15,7 @@ export default {
   footerEditOnGitHubLink: true, // will link to the docs repo
   logo: <>
     <svg>...</svg>
-    <span>Next.js Static Site Generator</span>
+    <span>Next Generation Markdown Editor</span>
   </>,
   head: <>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
